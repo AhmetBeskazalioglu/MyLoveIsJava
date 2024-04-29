22bafc46380fd504083b395714746bdded5e4128
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-04-29 16:09:46 +0300

    several code tutorials added

---
+++ src/codetraining/maaslar/Calisan.java
@@ -0,0 +1,16 @@
+package codetraining.maaslar;
+
+public class Calisan {
+    private double maas;
+
+    public Calisan() {
+    }
+
+    public Calisan(double maas) {
+        this.maas = maas;
+    }
+
+    public double getirMaas() {
+        return maas;
+    }
+}
