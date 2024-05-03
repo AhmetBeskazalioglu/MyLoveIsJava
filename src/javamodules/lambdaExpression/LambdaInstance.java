ec19dc50281cb08997e794a3521f69d4f7fde2ac
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-05-03 23:54:52 +0300

    Lambda Expressions Examples 2

--- src/javamodules/lambdaExpression/LambdaInstance.java
+++ src/javamodules/lambdaExpression/LambdaInstance.java
@@ -1,5 +1,7 @@
 package javamodules.lambdaExpression;
 
+import java.sql.SQLOutput;
+
 public class LambdaInstance {
 
     public static void main(String[] args) {
@@ -9,9 +11,64 @@ public class LambdaInstance {
         FuncInterface fobj = (int x) -> System.out.println(2 * x);
         FuncInterface fobj2 = (int x) -> System.out.println(x);
 
-        Selamlama merhaba= (x)-> System.out.println("Merhaba "+x);
-        Selamlama selam=(x)-> System.out.println("Selamun Aleyküm "+x);
-        Selamlama hi=(x)-> System.out.println("Hello "+x);
+        //Farkli dillerde selamlama
+        Selamlama merhaba = (x) -> System.out.println("Merhaba " + x);
+        Selamlama selam = (x) -> System.out.println("Selamun Aleyküm " + x);
+        Selamlama hi = (x) -> System.out.println("Hello " + x);
+
+
+        //Celcius, Fahrenheit and Kelvin temperature conversions
+       /* TemperatureConverter celciusToFahrenheit = (x) -> {
+            String start = "Celcius";
+            String end = "Fahrenheit";
+            System.out.println(x + " " + start + ", " + (x * 9 / 5 + 32) + " " + end);
+        };
+        TemperatureConverter fahrenheitToCelcius = (x) -> {
+            String start = "Fahrenheit";
+            String end = "Celcius";
+            System.out.println(x + " " + start + ", " + ((x - 32) * 5 / 9) + " " + end);
+        };
+        TemperatureConverter celciusToKelvin = (x) -> {
+            String start = "Celcius";
+            String end = "Kelvin";
+            System.out.println(x + " " + start + ", " + (x + 273.15) + " " + end);
+        };
+        TemperatureConverter kelvinToCelcius = (x) -> {
+            String start = "Kelvin";
+            String end = "Celcius";
+            System.out.println(x + " " + start + ", " + (x - 273.15) + " " + end);
+        };
+        TemperatureConverter fahrenheitToKelvin = (x) -> {
+            String start = "Fahrenheit";
+            String end = "Kelvin";
+            System.out.println(x + " " + start + ", " + ((x - 32) * 5 / 9 + 273.15) + " " + end);
+        };
+        TemperatureConverter kelvinToFahrenheit = (x) -> {
+            String start = "Kelvin";
+            String end = "Fahrenheit";
+            System.out.println(x + " " + start + ", " + ((x - 273.15) * 9 / 5 + 32) + " " + end);
+        };*/
+
+        TemperatureConverter converterAll = (x, start, end) -> {
+            if (start.toLowerCase().equals("celcius") && end.toLowerCase().equals("fahrenheit"))
+                System.out.println(x + " " + start + ", " + (x * 9 / 5 + 32) + " " + end);
+            else if (start.toLowerCase().equals("fahrenheit") && end.toLowerCase().equals("celcius"))
+                System.out.println(x + " " + start + ", " + ((x - 32) * 5 / 9) + " " + end);
+            else if (start.toLowerCase().equals("celcius") && end.toLowerCase().equals("kelvin"))
+                System.out.println(x + " " + start + ", " + (x + 273.15) + " " + end);
+            else if (start.toLowerCase().equals("kelvin") && end.toLowerCase().equals("celcius"))
+                System.out.println(x + " " + start + ", " + (x - 273.15) + " " + end);
+            else if (start.toLowerCase().equals("fahrenheit") && end.toLowerCase().equals("kelvin"))
+                System.out.println(x + " " + start + ", " + ((x - 32) * 5 / 9 + 273.15) + " " + end);
+            else if (start.toLowerCase().equals("kelvin") && end.toLowerCase().equals("fahrenheit"))
+                System.out.println(x + " " + start + ", " + ((x - 273.15) * 9 / 5 + 32) + " " + end);
+            else
+                System.out.println("Invalid start or end temperature type");
+        };
+
+        // Interface tipindeki değişkeni metota parametre olarak göndermek
+        Printer printer = (x, converter) -> converter.convert(x, "Celcius", "Fahrenheit");
+
 
         // This calls above lambda expression and prints 10.
         fobj.abstractFun(5);
@@ -20,6 +77,18 @@ public class LambdaInstance {
         merhaba.hi("Ahmet");
         selam.hi("Mehmet");
         hi.hi("Elif");
+
+        /*System.out.println("Celcius to Fahrenheit: "+celciusToFahrenheit.convert(0));
+        System.out.println("Fahrenheit to Celcius: "+fahrenheitToCelcius.convert(32));
+        System.out.println("Celcius to Kelvin: "+celciusToKelvin.convert(0));
+        System.out.println("Kelvin to Celcius: "+kelvinToCelcius.convert(273.15));
+        System.out.println("Fahrenheit to Kelvin: "+fahrenheitToKelvin.convert(32));
+        System.out.println("Kelvin to Fahrenheit: "+kelvinToFahrenheit.convert(273.15));*/
+
+        converterAll.convert(0, "Celcius", "Fahrenheit");
+        printer.print(0, converterAll);
+
+
     }
 }
 
@@ -31,6 +100,16 @@ interface FuncInterface {
 
 @FunctionalInterface
 interface Selamlama {
-
     void hi(String kimi);
+}
+
+@FunctionalInterface
+interface TemperatureConverter {
+    void convert(double x, String start, String end);
+}
+
+// Interface tipindeki değişkeni metota parametre olarak göndermek
+@FunctionalInterface
+interface Printer {
+    void print(double x, TemperatureConverter converter);
 }
