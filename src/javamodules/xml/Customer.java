36346fe1d44cdce15b67c06fa2b3f93e3ad6670c
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-05-07 17:52:56 +0300

    JAXB

---
+++ src/javamodules/xml/Customer.java
@@ -0,0 +1,50 @@
+package javamodules.xml;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Customer {
+
+    private String name;
+    private String surname;
+    private int age;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String surname, int age) {
+        super();
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [name=" + name + ", surname=" + surname + ", age=" + age + "]";
+    }
+}
