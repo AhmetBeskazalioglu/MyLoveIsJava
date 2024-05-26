c16c026260e6012cba3fc6c558a34c7253a8e81e
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-05-27 02:01:14 +0300

    Final Keywords update

--- src/javamodules/finalkeyword/FinalKeyword.java
+++ src/javamodules/finalkeyword/FinalKeyword.java
@@ -1,38 +1,60 @@
 package javamodules.finalkeyword;
 
-//04.05.24 Cumartesi
-
+/**
+ * @author Ahmet Beşkazalıoğlu
+ * 04.05.24 Cumartesi
+ */
 public class FinalKeyword {
+}
+
+class FinalKeyword_InMethod {
+
+    public void myMethod() {
+        final int m; // Metot içinde tanımlandı
+        m = 20; // Metot içinde assign ediliyor
+        System.out.println("m = " + m);
+    }
+}
+
+class FinalKeyword_InBlocks {
 
     static {
-        final int s = 30; // Static blok içinde tanımlandı
+        final int s; // Static blok içinde tanımlandı
+        s = 50; // Static blok içinde assign ediliyor
         System.out.println("s = " + s);
     }
 
     {
-        final int i = 30; // Instance blok içinde tanımlandı
+        final int i; // Instance blok içinde tanımlandı
+        i = 30; // Instance blok içinde assign ediliyor
         System.out.println("i = " + i);
     }
+}
 
+class FinalKeyword_InConstructor {
 
-    final int x; // Tanımlandığı yerde değer almak zorundadır
+    final int c; // Tanımlandığı yerde değer almak zorunda değildir.
 
-    public FinalKeyword() {
-        x = 10; // Constructor içinde assign ediliyor
+    public FinalKeyword_InConstructor() {
+        c = 40; // Constructor içinde assign ediliyor
+        System.out.println("c = " + c);
     }
+}
 
-    public void myMethod() {
-        final int y; // Metot içinde tanımlandı
-        y = 20; // Metot içinde assign ediliyor
-        System.out.println("y = " + y);
-    }
+class FinalKeyword_InClass {
+    final int clss = 5; // Tanımlandığı yerde değer almak zorundadır.
 }
 
 class Test {
     public static void main(String[] args) {
-        FinalKeyword obj = new FinalKeyword();
-        System.out.println(obj.x);
-        obj.myMethod();
+        FinalKeyword_InMethod obj1 = new FinalKeyword_InMethod();
+        obj1.myMethod();
 
+        FinalKeyword_InBlocks obj2 = new FinalKeyword_InBlocks();
+
+        FinalKeyword_InConstructor obj3 = new FinalKeyword_InConstructor();
+
+        FinalKeyword_InClass obj4 = new FinalKeyword_InClass();
+        System.out.println("clss = " + obj4.clss);
     }
 }
