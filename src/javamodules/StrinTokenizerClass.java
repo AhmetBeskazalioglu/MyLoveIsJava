d3aaea8c1d0b11270154ff4caae69a9ec10d8d6d
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-05-28 00:01:59 +0300

    StringTokenizer 4

--- src/javamodules/StrinTokenizerClass.java
+++ src/javamodules/StrinTokenizerClass.java
@@ -20,11 +20,32 @@ public class StrinTokenizerClass {
         System.out.println("*************************");
 
         String str2 = "Java StringTokenizer Class";
-
         java.util.StringTokenizer st2 = new java.util.StringTokenizer(str2, " ", true);
         while (st2.hasMoreTokens()) {
             System.out.println(st2.nextToken());
+        }
 
+        System.out.println("*************************");
+
+        String str3 = "Java,is,my,favourite,programming,language";
+        java.util.StringTokenizer st3 = new java.util.StringTokenizer(str3, ",");
+        while (st3.hasMoreTokens()) {
+            System.out.println(st3.nextToken());
+        }
+
+        System.out.println("*************************");
+
+        String str4 = "Java,is*,my,favourite*,programming,language*";
+        java.util.StringTokenizer st4 = new java.util.StringTokenizer(str4, ",*");
+        while (st4.hasMoreTokens()) {
+            System.out.println(st4.nextToken());
+        }
+
+        System.out.println("*************************");
+
+        java.util.StringTokenizer st5 = new java.util.StringTokenizer(str4, "*");
+        while (st5.hasMoreTokens()) {
+            System.out.println(st5.nextToken());
         }
 
     }
