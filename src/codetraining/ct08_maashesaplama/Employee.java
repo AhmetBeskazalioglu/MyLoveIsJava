94b7855ae3159787bf0aaaa92c7d2b8c90188352
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-06-05 05:01:44 +0300

    codeTraining

---
+++ src/codetraining/ct08_maashesaplama/Employee.java
@@ -0,0 +1,15 @@
+package codetraining.ct08_maashesaplama;
+
+public class Employee extends Employees{
+
+    public Employee(String name, double salary, int workHours, int hireYear) {
+        super(name, salary, workHours, hireYear);
+    }
+
+    @Override
+    public String toString() {
+        return "Adı: " + name + "\nMaaşı: " + salary + "\nÇalışma Saati: "
+                + workHours + "\nBaşlangıç Yılı: " + hireYear + "\nVergi: "
+                + tax() + "\nBonus: " + bonus() + "\nMaaş Artışı: " + raiseSalary();
+    }
+}
