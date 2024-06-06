cbf99ca6466db9fc1e3b877dfd4135a28c944640
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-06-06 22:01:54 +0300

    Proxy Design Pattern

---
+++ src/designpatterns/proxy/fileaccess/ProxyFileAccess.java
@@ -0,0 +1,20 @@
+package designpatterns.proxy.fileaccess;
+
+class ProxyFileAccess implements FileAccess {
+    private String fileName;
+    private User user;
+
+    public ProxyFileAccess(String fileName, User user) {
+        this.fileName = fileName;
+        this.user = user;
+    }
+
+    @Override
+    public void readFile(String fileName) {
+        if (user.isAdmin()) {
+            new RealFileAccess(fileName).readFile(fileName);
+        } else {
+            System.out.println("Access denied for user: " + user.getName());
+        }
+    }
+}
