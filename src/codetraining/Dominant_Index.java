869143cc12a0ecbdb84a7120fa115baa47525fb7
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-07-02 15:22:41 +0300

    Dominant_index

---
+++ src/codetraining/Dominant_Index.java
@@ -0,0 +1,33 @@
+package codetraining;
+
+public class Dominant_Index {
+    public int dominantIndex(int[] nums) {
+        int max=0;
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            if(max<nums[i]){
+                max=nums[i];
+                index=i;
+            }
+        }
+        int count=0;
+        for(int j=0;j<nums.length;j++){
+            if(j==index||nums[j]==0) {
+                count++;
+                continue;
+            }
+            else if((nums[index]/nums[j])>=2)
+                count++;
+        }
+        if(count==nums.length)
+            return index;
+        else
+            return -1;
+    }
+
+    public static void main(String[] args) {
+        Dominant_Index s = new Dominant_Index();
+        int[] nums = {3, 6, 1, 0};
+        System.out.println("Index of largest number: " + s.dominantIndex(nums));
+    }
+}
