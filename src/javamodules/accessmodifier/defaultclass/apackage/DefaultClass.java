bb49091da66ef9a1965e031b34feaa318511542c
AhmetBeskazalioglu <ahmetbeskazalioglu@icloud.com>
2024-11-27 18:10:20 +0300

    access modifier default and public sub package

---
+++ src/javamodules/accessmodifier/defaultclass/apackage/DefaultClass.java
@@ -0,0 +1,7 @@
+package javamodules.accessmodifier.defaultclass.apackage;
+
+public class DefaultClass {
+    static void publicMethod(){
+        System.out.println("Public method.");
+    }
+}
---
+++ src/javamodules/accessmodifier/defaultclass/bpackage/NormalClass.java
@@ -0,0 +1,9 @@
+package javamodules.accessmodifier.defaultclass.bpackage;
+
+import javamodules.accessmodifier.defaultclass.apackage.DefaultClass;
+
+public class NormalClass {
+    public static void main(String[] args) {
+       // DefaultClass.publicMethod();
+    }
+}
