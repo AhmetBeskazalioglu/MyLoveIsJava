1d2a259706fc1513ebfdbf01afd8794ce2d61cb1
AhmetBeskazalioglu <ahmetbeskazalioglu@icloud.com>
2024-12-01 23:55:16 +0300

    null pointer exception v1

--- src/exceptions/NullPointerException.java
+++ src/exceptions/NullPointerException.java
@@ -3,7 +3,7 @@ package exceptions;
 public class NullPointerException {
     public static void main(String[] args) {
         String str;
-        if(str.isEmpty())
+        // if(str.isEmpty())
         System.out.println("Boş bir String.");
     }
 }
