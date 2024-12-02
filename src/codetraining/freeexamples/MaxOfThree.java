0ba1f5faee13eed48005754d01b59640a726f8f8
AhmetBeskazalioglu <ahmetbeskazalioglu@icloud.com>
2024-12-02 16:33:34 +0300

    maxofthree

---
+++ src/codetraining/freeexamples/MaxOfThree.java
@@ -0,0 +1,33 @@
+package codetraining.freeexamples;
+
+import java.util.Scanner;
+
+public class MaxOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Kullanıcıdan giriş al
+        System.out.println("Birinci sayıyı giriniz:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("İkinci sayıyı giriniz:");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Üçüncü sayıyı giriniz:");
+        int num3 = scanner.nextInt();
+
+        // En büyük sayıyı bulma
+        int max;
+        if (num1 >= num2 && num1 >= num3) {
+            max = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            max = num2;
+        } else {
+            max = num3;
+        }
+
+        // Sonucu yazdır
+        System.out.println("En büyük sayı: " + max);
+    }
+}
+
