bb296bc96b42f0abdb36f44acf8da0c49a2e2d35
AhmetBeskazalioglu <ahmetbeskazalioglu@icloud.com>
2024-12-04 13:19:23 +0300

    Variable 'x' might not have been initialized

---
+++ src/codetraining/freeexamples/NotInitialized.java
@@ -0,0 +1,9 @@
+package codetraining.freeexamples;
+
+public class NotInitialized {
+    public static void main(String[] args){
+        int x;
+        // Variable 'x' might not have been initialized
+        // System.out.println(x);
+    }
+}
