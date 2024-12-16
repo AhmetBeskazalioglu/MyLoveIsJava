80d15ffb27d35d841fe8d41c7972f8011e277c74
AhmetBeskazalioglu <ahmetbeskazalioglu@icloud.com>
2024-12-16 23:48:40 +0300

    Access modifier on Classes1

---
+++ src/javamodules/accessmodifier/classmodifiers/pack1/A.java
@@ -0,0 +1,11 @@
+package javamodules.accessmodifier.classmodifiers.pack1;
+
+public class A
+{
+
+    public static int a = 10;
+
+    protected A() {
+        System.out.println("A constructor");
+    }
+}
---
+++ src/javamodules/accessmodifier/classmodifiers/pack2/B.java
@@ -0,0 +1,14 @@
+package javamodules.accessmodifier.classmodifiers.pack2;
+
+import javamodules.accessmodifier.classmodifiers.pack1.A;
+
+public class B
+{
+
+    int b = A.a;
+
+    //A a = new A(); // 'A()' has protected access in 'javamodules.accessmodifier.classmodifiers.pack1.A
+
+
+
+}
