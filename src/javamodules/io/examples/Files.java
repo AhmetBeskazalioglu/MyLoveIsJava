a9f693b5259bcd029ca42f4bbf97106acceb821a
AhmetBeskazalioglu <ahmetbeskazalioglu@icloud.com>
2024-12-18 21:36:12 +0300

    IO Files and scanner by files

--- src/javamodules/io/examples/Files.java
+++ src/javamodules/io/examples/Files.java
@@ -1,9 +1,10 @@
 package javamodules.io.examples;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Files {
-
         public static void main(String[ ] args) {
             File x = new File("/Users/anke/Desktop/my-time-machine/message.txt");
             if(x.exists()) {
@@ -12,6 +13,15 @@ public class Files {
             else {
                 System.out.println("The file does not exist");
             }
-        }
 
+            try {
+                Scanner sc = new Scanner(x);
+                while(sc.hasNextLine()) {
+                    System.out.println(sc.nextLine());
+                }
+            }
+            catch (FileNotFoundException e) {
+
+            }
+        }
 }
